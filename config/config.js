22a6a516b75eef3b93531be248fff6ec54cf03cd
NotProtocol <darknessgmn@gmail.com>
2026-01-16 23:40:39 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -11,7 +11,7 @@ module.exports = {
         port: process.env.DB_PORT || 3306,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
+        name: process.env.DB_NAME
     },
     bot: {
         prefix: process.env.PREFIX || '/',
