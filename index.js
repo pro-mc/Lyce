5b2e4910737be80a89174311670e7f83284816a3
NotProtocol <darknessgmn@gmail.com>
2026-01-16 12:53:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,88 @@
-const express = require('express');
-const app = express();
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const config = require('./config/config');
+const db = require('./src/database/db');
+const fs = require('fs');
+const path = require('path');
 
-const PORT = 5000;
-const HOST = '0.0.0.0';
+class LyceBot extends Client {
+    constructor() {
+        super({
+            intents: [
+                GatewayIntentBits.Guilds,
+                GatewayIntentBits.GuildMessages,
+                GatewayIntentBits.MessageContent,
+                GatewayIntentBits.GuildMembers,
+                GatewayIntentBits.GuildModeration,
+                GatewayIntentBits.GuildVoiceStates
+            ]
+        });
 
-app.get('/', (req, res) => {
-  res.send('<h1>Welcome to Lyce</h1><p>Your application is running!</p>');
+        this.config = config;
+        this.commands = new Collection();
+        this.cooldowns = new Collection();
+        this.db = db;
+    }
+
+    async initialize() {
+        await this.db.initialize();
+        await this.loadCommands();
+        await this.loadEvents();
+        
+        this.login(this.config.discord.token).then(() => {
+            console.log(`${this.config.bot.name} v${this.config.bot.version} is online!`);
+        }).catch(console.error);
+    }
+
+    async loadCommands() {
+        const commandsPath = path.join(__dirname, 'src', 'commands');
+        const commandFolders = fs.readdirSync(commandsPath);
+
+        for (const folder of commandFolders) {
+            const folderPath = path.join(commandsPath, folder);
+            const commandFiles = fs.readdirSync(folderPath).filter(file => file.endsWith('.js'));
+
+            for (const file of commandFiles) {
+                const filePath = path.join(folderPath, file);
+                const command = require(filePath);
+                
+                if ('data' in command && 'execute' in command) {
+                    this.commands.set(command.data.name, command);
+                    console.log(`Loaded command: ${command.data.name}`);
+                } else {
+                    console.log(`Command ${filePath} is missing required properties`);
+                }
+            }
+        }
+    }
+
+    async loadEvents() {
+        const eventsPath = path.join(__dirname, 'src', 'events');
+        const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+        for (const file of eventFiles) {
+            const filePath = path.join(eventsPath, file);
+            const event = require(filePath);
+            
+            if (event.once) {
+                this.once(event.name, (...args) => event.execute(...args, this));
+            } else {
+                this.on(event.name, (...args) => event.execute(...args, this));
+            }
+            
+            console.log(`Loaded event: ${event.name}`);
+        }
+    }
+}
+
+const bot = new LyceBot();
+bot.initialize();
+
+process.on('unhandledRejection', error => {
+    console.error('Unhandled promise rejection:', error);
 });
 
-app.listen(PORT, HOST, () => {
-  console.log(`Server running at http://${HOST}:${PORT}`);
+process.on('SIGINT', async () => {
+    console.log('Shutting down...');
+    await bot.db.pool.end();
+    process.exit(0);
 });
